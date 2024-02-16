cea8e926eab914f57131f4ded9f100512c7adfba
Borlaff <asborlaff@gmail.com>
2024-02-16 09:43:49 -0800

    Randominzing temporary name of image_trace and parameter_trace

--- pylira/core.py
+++ pylira/core.py
@@ -18,6 +18,7 @@ from .utils.plot import (
     plot_pixel_trace,
     plot_pixel_trace_neighbours,
 )
+import string
 
 DTYPE_DEFAULT = np.float64
 
@@ -173,8 +174,12 @@ class LIRADeconvolver:
         random_seed = self.random_state.randint(1, np.iinfo(np.uint32).max)
 
         with tempfile.TemporaryDirectory() as tmpdir:
-            filename_image_trace = str(os.path.join(tmpdir, "image-trace.tx"))
-            filename_parameter_trace = str(os.path.join(tmpdir, "parameter-trace.txt"))
+            # A.S. Borlaff - NASA/Ames, Feb 16 2024. 
+            # Adding a randomized temporary name to the image_trace and parameter_trace files 
+            # So the do not overlap if multiple LIRAs are launched in parallel. 
+            random_code_string = join(random.choices(string.ascii_uppercase, k=10))
+            filename_image_trace = str(os.path.join(tmpdir, "image-trace_"+ random_code_string +".txt"))
+            filename_parameter_trace = str(os.path.join(tmpdir, "parameter-trace_" + random_code_string + ".txt"))
 
             posterior_mean = image_analysis(
                 observed_im=data["counts"],
